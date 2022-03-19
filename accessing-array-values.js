f2bfb0609d584a1d868c13b069193d1222ccaeeb
Chanella Cortez <chanellacortez@gmail.com>
2022-03-18 23:31:50 -0700

    Initial commit with all my work so far

---
+++ accessing-array-values.js
@@ -0,0 +1,3 @@
+let food = ['apple', 'pizza' , 'pear'];
+// prints index 2 
+console.log(food[1]);
---
+++ for-loop.js
@@ -0,0 +1,10 @@
+// total amount
+let total = 0
+// limit amount
+let limit = 10
+
+for (let i = 0; i < limit; i++) {
+    total += i
+}
+
+console.log(total);
